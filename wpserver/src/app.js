abd58bad1b2df67d0d249d1bd7bdd519e7df4960
Mike Puglisi <m.ikepuglisi@gmail.com>
2018-05-21 19:16:35 -0400

    Refactor

--- wpserver/src/app.js
+++ wpserver/src/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Routes
-console.log('routes', routes)
+
 app.use('/', routes);
 
 // Catch 404 and forward to error handler
